b91b578914859de39ad7292a1117bc04c3f67151
Kacper Niciak <ciaparino123@gmail.com>
2024-06-17 19:38:02 +0200

    dynamiczna alokacja pamieci przyklad

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    int i,aSize;
+    int * randomNums;
+
+    double total = 0;
+    int biggest,smallest;
+    float average;
+
+    printf("ile liczb losowych chcesz w tablicy??");
+
+    return 0;
+}
